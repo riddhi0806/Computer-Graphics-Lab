a127b77564dd47efb7f86ec14ede87890dbf0f16
riddhi0806 <33753283+riddhi0806@users.noreply.github.com>
2019-09-04 23:36:22 +0530

    Update hut.c

--- hut.c
+++ hut.c
@@ -1,129 +1,64 @@
-#include<GL/glut.h>
-#include<stdio.h>
-//
-// void drawHut()
-// {
-//   // glClearColor(1.0,1.0,1.0,1.0); /*specifies red,green,blue and alpha (opacity) values used by glclear to clear the color buffers*/
-//   glClear(GL_COLOR_BUFFER_BIT);/*performs clear operation on one or more buffers at the same time*/
-//
-//   glColor3f(0.0,2.0,0.5);/* indicates the color for red, green and blue*/
-//   //glOrtho(-1.0,1.0,-1.0,1.0,-1.0,1.0);/* indicates the clipping value for left, right , bottom, top, nearest and farthest value*/
-//   glBegin(GL_POLYGON);
-//   glVertex2f(-0.4,0.5);
-//   glVertex2f(-0.75,0);
-//   glVertex2f(0,0);
-//   glEnd();
-//
-//   glColor3f(2.0,0.0,0.5);
-//   glBegin(GL_POLYGON);
-//   glVertex2f(-0.4,0.5);
-//   glColor3f(0.0,1.0,0.0);
-//   glVertex2f(0,0.4);
-//   glVertex2f(0.4,0);
-//   //glColor3f(2.0,0.0,0.5);
-//   glVertex2f(0.7,0.5);
-//   glVertex2f(1,0);
-//   glVertex2f(0,0);
-//   glEnd();
-//
-//   glColor3f(2.0,2.0,0.0);
-//   glBegin(GL_POLYGON);
-//   glVertex2f(-0.75,0);
-//   glVertex2f(-0.75,-0.7);
-//   glVertex2f(0,-0.7);
-//   glVertex2f(0,0);
-//   glEnd();
-//
-//   glColor3f(0.5,1.0,1.5);
-//   glBegin(GL_POLYGON);
-//   glVertex2f(0,0);
-//   glVertex2f(1,0);
-//   glVertex2f(1,-0.7);
-//   glVertex2f(0,-0.7);
-//   glEnd();
-//
-//   glFlush();
-// }
-//
-// int main (int argc, char** argv){
-//   glutInit(&argc,argv); //initializes the OpenGL Utility toolkit
-//   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);//set the display mode
-//   glutInitWindowPosition(300,300);//set the window position on the screen
-//   glutCreateWindow("Drawing Hut"); //Decides the title of the window
-//   glutDisplayFunc(drawHut); //screen window is redrawn
-//   glutInitWindowSize(700,500);//set the window size
-//   glutMainLoop();
-// }
-// =======
-void myinit(void)
-{
- glClearColor(1.0,1.0,1.0,0.0);/*specifies red,green,blue and alpha (opacity) values used by glclear to clear the color buffers*/
- glMatrixMode(GL_PROJECTION);
- glLoadIdentity();
- glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);/* indicates the clipping value for left, right , bottom, top, nearest and farthest value*/
- }
-
- void hut(void)
- {
-glClear(GL_COLOR_BUFFER_BIT);/*performs clear operation on one or more buffers at the same time*/
-glColor3f(0.1,0.2,0.3); /* indicates the color for red, green and blue*/
-//rectangle
-	glBegin(GL_POLYGON);
-  	glVertex2f (0.1, 0.1);
-    glVertex2f (0.4, 0.1);
-    glVertex2f (0.4, 0.5);
-    glVertex2f (0.1, 0.5);
-  glEnd();
-glColor3f(1.0,0.2,0.1);
-//triangle
-	glBegin(GL_POLYGON);
-	glVertex2f (0.10, 0.5);
-        glVertex2f (0.4, 0.5);
-        glVertex2f (0.25, 0.75);
-       glEnd();
+#include <GL/glut.h> 
+void renderFunction() 
+{ 
+glClearColor(0.0,0.0,0.0,0.0); /*specifies red,green,blue and alpha (opacity) values used by glclear to clear the color buffers*/ 
+glClear(GL_COLOR_BUFFER_BIT);/*performs clear operation on one or more buffers at the same time*/ 
+glColor3f(1.0,1.0,0.0);/* indicates the color for red, green and blue*/ 
+//glOrtho(-1.0,1.0,-1.0,1.0,-1.0,1.0);/* indicates the clipping value for left, right , bottom, top, nearest and farthest value*/ 
+glBegin(GL_POLYGON); 
+glVertex2f(-0.5,-0.5);/*origin of line with two vertices of float */ 
+glVertex2f(-0.5,0.0); 
+glVertex2f(0.0,0.0); 
+glVertex2f(0.0,-0.5);/* representing square*/ 
+glEnd(); 
+glColor3f(0.4,0.2,0.1);
+glBegin(GL_POLYGON); 
+glVertex2f(-0.25,0.5); 
+glVertex2f(-0.5,0.0); 
+glVertex2f(0.0,0.0);//represents triangle 
+glEnd(); 
 glColor3f(0.2,1.0,0.2);
-//right rectangle
-	glBegin(GL_POLYGON);
-	glVertex2f (0.4, 0.1);
-        glVertex2f (0.8, 0.4);
-        glVertex2f (0.8, 0.75);
-        glVertex2f (0.4, 0.5);
-    glEnd();
-glColor3f(0.4,0.2,0.4);
-//upper rectangle
-	glBegin(GL_POLYGON);
-	glVertex2f (0.4, 0.5);
-        glVertex2f (0.8, 0.75);
-        glVertex2f (0.62, 0.93);
-        glVertex2f (0.25, 0.75);
-    glEnd();
+glBegin(GL_POLYGON); 
+glVertex2f(-0.15,-0.25);/*origin of line with two vertices of float */ 
+glVertex2f(-0.35,-0.25); 
+glVertex2f(-0.35,-0.5); 
+glVertex2f(-0.15,-0.5);/* representing gate*/ 
+glEnd();
+glColor3f(1.0,0.0,0.2);
+glBegin(GL_POLYGON);
+glVertex2f(-0.25,0.5);/*origin of line with two vertices of float */ 
+glVertex2f(0.75,0.5); 
+glVertex2f(1.0,0.0); 
+glVertex2f(0.0,0.0);
 
-glColor3f(0.2,1.0,0.2);
-//inner rectangle
-	glBegin(GL_POLYGON);
-	glVertex2f (0.15, 0.25);
-        glVertex2f (0.25, 0.25);
-        glVertex2f (0.25, 0.35);
-        glVertex2f (0.15, 0.35);
-    glEnd();
-glColor3f(0.22,0.12,0.14);
-	glBegin(GL_POLYGON);
-	glVertex2f (0.6, 0.25);
-        glVertex2f (0.7, 0.3);
-        glVertex2f (0.6, 0.25);
-        glVertex2f (0.7, 0.25);
-    glEnd();
-glFlush();
-}
-int main(int argc,char** argv)
-{
-glutInit(&argc,argv);/*initialize the toolkit*/
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);/*set the display mode*/
-glutInitWindowPosition(50,100);//set the window position on the screen
-glutInitWindowSize(640,480);//set the window size
-glutCreateWindow("hut");//open window with a title
-myinit();
-glutDisplayFunc(hut);//screen window is redrawn
-glutMainLoop();
-// >>>>>>> e300a7c5b8ed03ac20b5629c6d5ddc0f2ac22093
+glEnd(); 
+glColor3f(0.4,1.0,0.4);
+glBegin(GL_POLYGON);
+glVertex2f(0.0,-0.5);/*origin of line with two vertices of float */ 
+glVertex2f(1.0,-0.5); 
+glVertex2f(1.0,0.0); 
+glVertex2f(0.0,0.0);
+glColor3f(1.0,0.2,1.0);
+
+glEnd(); 
+glBegin(GL_POLYGON); 
+glVertex2f(0.25,-0.15);/*origin of line with two vertices of float */ 
+glVertex2f(0.65,-0.15); 
+glVertex2f(0.25,-0.35); 
+glVertex2f(0.65,-0.35);/* representing window(square)*/ 
+glEnd(); 
+glColor3f(0.4,0.2,0.1);
+
+
+glFlush(); 
+} 
+int main (int argc, char** argv) 
+{ 
+glutInit(&argc,argv); /*initialize the toolkit*/ 
+glutInitDisplayMode(GLUT_SINGLE);/*set the display mode*/ 
+glutInitWindowSize(1000,800);//set the window size 
+glutInitWindowPosition(100,100);//set the window position on the screen 
+glutCreateWindow("Hut"); //open window with a title 
+glutDisplayFunc(renderFunction); //screen window is redrawn 
+glutMainLoop(); 
 }
